c510a598d05f902c3e596caa8d660f43a8f615bc
Khari Johnson <kj@gvempire.com>
2020-12-07 18:23:58 -0500

    add SEO and marketing images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,17 +23,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="application-name" content="Shrt | A GVEMPIRE product" />
+          <meta name="application-name" content="Shrt" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
+          <meta name="apple-mobile-web-app-title" content="Shrt" />
           <meta
-            name="apple-mobile-web-app-title"
-            content="Shrt | A GVEMPIRE product"
+            name="description"
+            content="URL Shortener and Personal Landing Page by GVEMPIRE"
           />
-          <meta name="description" content="TM1 from Shrt" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta
@@ -43,6 +43,31 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://shrtme.app" />
+          <meta name="twitter:title" content="Shrt" />
+          <meta
+            name="twitter:description"
+            content="URL Shortener and Personal Landing Page by GVEMPIRE"
+          />
+          <meta
+            name="twitter:image"
+            content="https://shrtme.app/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@tkjohnson121" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Shrt" />
+          <meta
+            property="og:description"
+            content="URL Shortener and Personal Landing Page by GVEMPIRE"
+          />
+          <meta property="og:site_name" content="Shrt" />
+          <meta property="og:url" content="https://shrtme.app" />
+          <meta
+            property="og:image"
+            content="https://shrtme.app/icons/apple-touch-icon.png"
+          />
 
           <link
             rel="apple-touch-icon"
@@ -68,24 +93,6 @@ export default class MyDocument extends Document {
             color="#5bbad5"
           />
           <link rel="shortcut icon" href="/icons/favicon.ico" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://Shrt.gvempire.dev" />
-          <meta name="twitter:title" content="Shrt | A GVEMPIRE product" />
-          <meta name="twitter:description" content="Shrt from GVEMPIRE" />
-          <meta
-            name="twitter:image"
-            content="https://Shrt.gvempire.dev/icons/android-chrome-192x192.png"
-          />
-          <meta name="twitter:creator" content="@DavidWShadow" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Shrt | A GVEMPIRE product" />
-          <meta property="og:description" content="TM1 from Shrt" />
-          <meta property="og:site_name" content="Shrt | A GVEMPIRE product" />
-          <meta property="og:url" content="https://Shrt.gvempire.dev" />
-          <meta
-            property="og:image"
-            content="https://Shrt.gvempire.dev/icons/apple-touch-icon.png"
-          />
         </Head>
         <body>
           <Main />
