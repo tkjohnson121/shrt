3059c5a4646afb15a6f67a89225aba61cb2c9ebe
Khari Johnson <kj@gvempire.com>
2020-12-12 15:07:15 -0500

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/core';
-import { ShrtForm } from 'common';
 import { Theme } from 'features/theme';
+import Link from 'next/link';
 import React from 'react';
 
 const styles = {
@@ -12,25 +12,60 @@ const styles = {
 
     .tagline {
       font-weight: ${theme.fontWeights['light']};
-      font-size: ${theme.fontSizes['2xl']};
+      font-size: ${theme.fontSizes['xl']};
       max-width: 50ch;
+      margin: 0 auto ${theme.space[4]};
+      line-height: ${theme.lineHeights['tall']};
+    }
+
+    .features {
+      max-width: 50ch;
+      list-style-type: circle;
+      text-align: left;
       margin: 0 auto;
+      padding: 0;
+
+      li {
+        margin-bottom: ${theme.space[4]};
+
+        a {
+          border-bottom: 2px solid ${theme.colors['primary']};
+          background-color: transparent;
+          color: ${theme.colors.whiteAlpha[900]};
+          font-weight: ${theme.fontWeights['semibold']};
+        }
+      }
     }
   `,
 };
 
 export default function LandingPage() {
   return (
-    <>
-      <section css={styles.section}>
-        <h1 className="display">Welcome to SHRT!</h1>
-        <p className="tagline">
-          Personal Landing Page (PLP) and URL-Shortner that allows you to
-          connect faster and network more efficiently.
-        </p>
+    <section css={styles.section}>
+      <h1 className="display">SHRT!</h1>
+      <p className="tagline">Your Main Link, Forever.</p>
 
-        <ShrtForm />
-      </section>
-    </>
+      <ul className="features">
+        <li>
+          <p>
+            Keep all your side links in one place and connect with people on any
+            platform with{' '}
+            <Link href="/user/settings">
+              <a>Personal Landing Pages (PLP)</a>
+            </Link>
+            .
+          </p>
+        </li>
+        <li>
+          <p>
+            Share any link and easily track it's usage and analytics with{' '}
+            <Link href="/user/dashboard">
+              <a>Shrts</a>
+            </Link>
+            .
+          </p>
+        </li>
+      </ul>
+    </section>
   );
 }
